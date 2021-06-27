ceddc3176558c880458341b5883367f43aaab9f0
greats3an <greats3an@gmail.com>
2021-06-27 16:33:49 +0800

    Version 1.0.0.2 [+] update `localfile` now supports uploading directories

--- bilibili_toolman/providers/localfile.py
+++ bilibili_toolman/providers/localfile.py
@@ -4,7 +4,7 @@ from . import DownloadResult
 __desc__ = '本地文件'
 __cfg_help__ = '''
     cover (str) - 封面图片路径    
-e.g. --localfile "le video.mp4" --opts cover="le cover.png" --tags ...'''
+e.g. --localfile "le videos/" --opts cover="le cover.png" --tags ...'''
 options={
     'cover':''
 }
@@ -15,13 +15,18 @@ def update_config(opt):
 
 def download_video(res) -> DownloadResult:
     results = DownloadResult()
-    if not os.path.isfile(res) or (options['cover'] and os.path.isfile(options['cover'])):
+    if not os.path.isfile(res) and not os.path.isdir(res) or (options['cover'] and os.path.isfile(options['cover'])):
         raise FileNotFoundError
-    with DownloadResult() as result:        
-        result.video_path = res
-        result.cover_path = options['cover']            
-        result.original = True    
-        result.title = os.path.basename(res)            
-        result.description = '[automated upload of file %s]' % res        
-    results.results.append(result)
+    def append(res):        
+        with DownloadResult() as result:        
+            result.video_path = res
+            result.cover_path = options['cover']            
+            result.original = True    
+            result.title = os.path.basename(res)            
+            result.description = '[automated upload of file %s]' % res        
+        results.results.append(result)
+    if os.path.isfile(res):append(res)
+    else:
+        for f in os.listdir(res):
+            append(os.path.join(res,f))    
     return results
